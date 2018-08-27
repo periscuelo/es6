327b21f91ec7c4ab70a6c2fd15b16023b0a72e24
unknown <periscuelo@gmail.com>
2018-08-27 16:34:19 -0300

    ajuste

--- app/index.js
+++ app/index.js
@@ -6,8 +6,7 @@ console.dir(new Message());
 /* eslint no-undef: 0 */
 document.getElementById('send').onclick = () => {
   const m = new Message(document.getElementById('message').value);
-  document.getElementById('messages').innerHTML +=
-    `<li>${m.text} ${m.created}</li>`;
+  document.getElementById('messages').innerHTML += `<li>${m.text} ${m.created}</li>`;
 };
 
 if (module && module.hot) {
