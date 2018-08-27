f05833d54e4099835492520416e727f8dedef5d8
unknown <periscuelo@gmail.com>
2018-08-27 16:15:48 -0300

    Acrescentado hot reload

--- app/index.js
+++ app/index.js
@@ -2,3 +2,13 @@ import Message from './model/message.model';
 
 console.log('Index started');
 console.dir(new Message());
+
+/* eslint no-undef: 0 */
+document.querySelector('#send').onclick = () => {
+  const m = new Message(document.querySelector('#message').value);
+  document.querySelector('#messages').innerHTML += `<li>${m.text} ${m.created}</li>`;
+};
+
+if (module && module.hot) {
+  module.hot.accept();
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 const config = {
   entry: './app/index.js',
@@ -13,6 +14,14 @@ if (process.env.NODE_ENV === 'development') {
   config.mode = 'development';
   config.watch = true;
   config.devtool = 'source-map';
+} else if (process.env.NODE_ENV === 'hot') {
+  config.devtool = 'source-map';
+  config.devServer = {
+    hot: true,
+  };
+  config.plugins = [
+    new webpack.HotModuleReplacementPlugin(),
+  ];
 } else if (process.argv[process.argv.indexOf('--mode') + 1] === 'development') {
   config.watch = true;
   config.devtool = 'source-map';
