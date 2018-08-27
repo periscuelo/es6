a884230e079c27b169cb8456826a55fe35b92d63
unknown <periscuelo@gmail.com>
2018-08-27 14:28:29 -0300

    Ajustado arquivos para funcionar no Windows

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,15 @@ const config = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
+    publicPath: 'dist/',
   },
 };
 
 if (process.env.NODE_ENV === 'development') {
+  config.mode = 'development';
+  config.watch = true;
+  config.devtool = 'source-map';
+} else if (process.argv[process.argv.indexOf('--mode') + 1] === 'development') {
   config.watch = true;
   config.devtool = 'source-map';
 }
