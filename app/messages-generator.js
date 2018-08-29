25d692989de2ab63e520bb6885af04e8c9834975
unknown <periscuelo@gmail.com>
2018-08-29 11:33:37 -0300

    Ajuste

--- app/messages-generator.js
+++ app/messages-generator.js
@@ -4,9 +4,7 @@ const generator = () => {
   const messages = [];
   for (let x = 0; x < 50; x += 1) {
     messages.push(
-      new Message(`Message ${x + 1}`,
-                  Date.now() - parseInt(Math.random() * 2000000, 10),
-      ),
+      new Message(`Message ${x + 1}`, Date.now() - parseInt(Math.random() * 2000000, 10)),
     );
   }
   return messages;
